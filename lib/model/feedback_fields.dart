1ae4739478908be4b5c4b84a6263271060a91c38
angkeasel <angkeaselsouk@gmail.com>
2024-06-03 13:51:05 +0700

    field model

--- lib/model/feedback_fields.dart
+++ lib/model/feedback_fields.dart
@@ -3,6 +3,7 @@ class FeedbackFields {
   static const String date = 'Date';
   static const String reason = 'reason';
   static const String location = 'location';
+  static const String comments = 'comments';
 
-  List<String> getFields() => [feedback, date, reason, location];
+  List<String> getFields() => [feedback, date, reason, location, comments];
 }
